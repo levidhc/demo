9583d2497c7ad25232337fcad560ae017456987d
HP <levidhc@gmail.com>
2023-01-30 02:11:10 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/HpPrinter.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class HpPrinter implements Printer {
+    private int count;
+    @PostConstruct
+    public void init(){
+        count = 5;
+    }
+    @Override
+    public void print(String message){
+        count--;
+        System.out.println("HP印表機"+message);
+        System.out.println("剩餘："+count);
+    }
+}
